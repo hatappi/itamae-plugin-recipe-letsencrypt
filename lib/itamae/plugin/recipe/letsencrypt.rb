bb4aabcba5074e916cdc8aff03437021c0458851
yusaku.hatanaka <hata.yusaku.1225@gmail.com>
2016-12-18 13:50:30 +0900

    delete nesessary logic

--- lib/itamae/plugin/recipe/letsencrypt.rb
+++ lib/itamae/plugin/recipe/letsencrypt.rb
@@ -1,11 +1,1 @@
 require "itamae/plugin/recipe/letsencrypt/version"
-
-module Itamae
-  module Plugin
-    module Recipe
-      module Letsencrypt
-        # Your code goes here...
-      end
-    end
-  end
-end
