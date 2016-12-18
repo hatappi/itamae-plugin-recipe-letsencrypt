d31cb703ce51cdeab8515764a1b77ee836801960
yusaku.hatanaka <hata.yusaku.1225@gmail.com>
2016-12-18 12:04:54 +0900

    firt commit

---
+++ lib/itamae/plugin/recipe/letsencrypt.rb
@@ -0,0 +1,11 @@
+require "itamae/plugin/recipe/letsencrypt/version"
+
+module Itamae
+  module Plugin
+    module Recipe
+      module Letsencrypt
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ lib/itamae/plugin/recipe/letsencrypt/version.rb
@@ -0,0 +1,9 @@
+module Itamae
+  module Plugin
+    module Recipe
+      module Letsencrypt
+        VERSION = "0.1.0"
+      end
+    end
+  end
+end
