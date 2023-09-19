7ba123753fdfa81b9c6239c99dd1b9f530a3dd7b
gemmaro <gemmaro.dev@gmail.com>
2023-09-20 00:04:29 +0900

    Use shellwords for get recipe

--- lib/itamae/plugin/recipe/letsencrypt/get.rb
+++ lib/itamae/plugin/recipe/letsencrypt/get.rb
@@ -1,3 +1,5 @@
+require 'shellwords'
+
 node.reverse_merge!(
   letsencrypt: {
     certbot_auto_path: '/usr/bin/certbot-auto',
@@ -11,20 +13,29 @@ node.reverse_merge!(
 )
 
 execute 'download certbot-auto' do
-  command "wget https://dl.eff.org/certbot-auto -O #{node[:letsencrypt][:certbot_auto_path]}"
-  not_if "test -f #{node[:letsencrypt][:certbot_auto_path]}"
+  download = ["wget", "https://dl.eff.org/certbot-auto", "-O", node[:letsencrypt][:certbot_auto_path]].shelljoin
+  command download
+
+  exists = ["test", "-f", node[:letsencrypt][:certbot_auto_path]].shelljoin
+  not_if exists
 end
 
 execute 'change certbot-auto permission' do
-  command "chmod a+x #{node[:letsencrypt][:certbot_auto_path]}"
-  not_if "test -x #{node[:letsencrypt][:certbot_auto_path]}"
+  make_executable = ["chmod", "a+x", node[:letsencrypt][:certbot_auto_path]].shelljoin
+  command make_executable
+
+  executable_exists = ["test", "-x", node[:letsencrypt][:certbot_auto_path]].shelljoin
+  not_if executable_exists
 end
 
 execute 'install dependency package' do
-  cmd = "#{node[:letsencrypt][:certbot_auto_path]} -n --os-packages-only"
-  cmd << ' --debug' if node[:letsencrypt][:debug_mode]
-  command cmd
-  not_if "test -n \"$(#{cmd} --dry-run | grep 'OS packages installed.')\""
+  cmd = [node[:letsencrypt][:certbot_auto_path], "-n", "--os-packages-only"].shelljoin
+  cmd << '--debug' if node[:letsencrypt][:debug_mode]
+  command cmd.shelljoin
+
+  dry_run = [*cmd, '--dry-run'].shelljoin
+  detect = ["grep", "OS packages installed."].shelljoin
+  not_if "test -n $(#{dry_run} | #{detect})"
 end
 
 # get each domain certificate
@@ -34,17 +45,20 @@ node[:letsencrypt][:domains].each do |domain|
       node[:letsencrypt][:certbot_auto_path],
       'certonly',
       '--agree-tos',
-      "-d #{domain}",
-      "-m #{node[:letsencrypt][:email]}",
-      "-a #{node[:letsencrypt][:authenticator]}",
+      "-d", domain,
+      "-m", node[:letsencrypt][:email],
+      "-a", node[:letsencrypt][:authenticator],
       '--keep',
       '-n',
-      "--preferred-challenges #{node[:letsencrypt][:challenge_type]}",
+      "--preferred-challenges", node[:letsencrypt][:challenge_type],
     ]
-    cmd << "-w #{node[:letsencrypt][:webroot_path]}" if node[:letsencrypt][:webroot_path]
+    cmd += ["-w", node[:letsencrypt][:webroot_path]] if node[:letsencrypt][:webroot_path]
     cmd << '--debug' if node[:letsencrypt][:debug_mode]
-    command cmd.join(' ')
-    not_if "test -d /etc/letsencrypt/live/#{domain}"
+    command cmd.shelljoin
+
+    dir = File.join("/etc/letsencrypt/live", domain)
+    exists = ["test", "-d", dir].shelljoin
+    not_if exists
   end
 end
 
